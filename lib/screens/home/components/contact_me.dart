920d98dcd46eeb024764b552b11f920f4c1c378e
Daffa Ilhami <mdaffailhami@gmail.com>
2022-02-14 21:41:38 +0800

    Memfungsikan Contact Me Form

--- lib/screens/home/components/contact_me.dart
+++ lib/screens/home/components/contact_me.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class MyContactMeForm extends StatelessWidget {
+class MyContactMeForm extends StatefulWidget {
   const MyContactMeForm({Key? key}) : super(key: key);
 
   static final GlobalKey componentKey = GlobalKey();
 
   @override
-  Key? get key => componentKey;
+  Key? get key => MyContactMeForm.componentKey;
+
+  @override
+  State<MyContactMeForm> createState() => _MyContactMeFormState();
+}
+
+class _MyContactMeFormState extends State<MyContactMeForm> {
+  String _name = '';
+  String _email = '';
+  String _message = '';
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +33,10 @@ class MyContactMeForm extends StatelessWidget {
             'Contact Me',
             style: Theme.of(context).textTheme.headline4,
           ),
-          Divider(),
+          const Divider(),
           Container(
-            constraints: BoxConstraints(maxWidth: 500),
-            decoration: BoxDecoration(
+            constraints: const BoxConstraints(maxWidth: 500),
+            decoration: const BoxDecoration(
               color: Colors.black54,
               borderRadius: BorderRadius.all(Radius.circular(10)),
             ),
@@ -37,25 +47,34 @@ class MyContactMeForm extends StatelessWidget {
                 children: [
                   TextFormField(
                     keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       prefixIcon: Icon(Icons.person),
                       labelText: 'Name',
                     ),
+                    onChanged: (String value) {
+                      _name = value;
+                    },
                   ),
                   TextFormField(
                     keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       prefixIcon: Icon(Icons.email),
                       labelText: 'Email address',
                     ),
+                    onChanged: (String value) {
+                      _email = value;
+                    },
                   ),
                   TextFormField(
                     maxLines: null,
                     keyboardType: TextInputType.multiline,
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       prefixIcon: Icon(Icons.message),
                       labelText: 'Message',
                     ),
+                    onChanged: (String value) {
+                      _message = value;
+                    },
                   ),
                   SizedBox(
                     width: double.infinity,
@@ -64,8 +83,67 @@ class MyContactMeForm extends StatelessWidget {
                       style: ElevatedButton.styleFrom(
                         primary: Theme.of(context).colorScheme.primary,
                       ),
-                      onPressed: () {},
-                      child: Text('SEND'),
+                      onPressed: () async {
+                        final Uri url = Uri.parse(
+                          'https://mdaffailhami.herokuapp.com/api/contact-me',
+                        );
+
+                        final Map data = {
+                          'name': _name,
+                          'email': _email,
+                          'message': _message,
+                        };
+
+                        try {
+                          await http.post(url, body: data);
+
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              duration: const Duration(seconds: 10),
+                              content: const Text(
+                                'Message sent!',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              backgroundColor: Colors.green,
+                              action: SnackBarAction(
+                                label: 'Dismiss',
+                                textColor:
+                                    Theme.of(context).colorScheme.secondary,
+                                onPressed: () {
+                                  ScaffoldMessenger.of(context)
+                                      .hideCurrentSnackBar();
+                                },
+                              ),
+                            ),
+                          );
+                        } catch (e) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              duration: const Duration(seconds: 10),
+                              content: Text(
+                                'Failed to send message!',
+                                style: TextStyle(
+                                  color: Theme.of(context).colorScheme.onError,
+                                ),
+                              ),
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.error,
+                              action: SnackBarAction(
+                                label: 'Dismiss',
+                                textColor:
+                                    Theme.of(context).colorScheme.secondary,
+                                onPressed: () {
+                                  ScaffoldMessenger.of(context)
+                                      .hideCurrentSnackBar();
+                                },
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                      child: const Text('SEND'),
                     ),
                   )
                 ],
