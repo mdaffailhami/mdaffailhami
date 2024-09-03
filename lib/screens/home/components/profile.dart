9bec6540b509170bd48d7d4b1d16513561afda7c
mdaffailhami <mdaffailhami@gmail.com>
2024-09-03 08:41:51 +0800

    Update profile.dart

--- lib/screens/home/components/profile.dart
+++ lib/screens/home/components/profile.dart
@@ -3,46 +3,40 @@ import 'package:flutter/material.dart';
 class MyProfile extends StatelessWidget {
   const MyProfile({Key? key}) : super(key: key);
 
+  TextSpan getDivider(BuildContext context) {
+    return TextSpan(
+      text: ' | ',
+      style: TextStyle(color: Theme.of(context).colorScheme.secondaryContainer),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         const CircleAvatar(
-          radius: 74,
+          radius: 84,
           backgroundColor: Colors.white,
           child: CircleAvatar(
             backgroundImage: AssetImage('assets/profile.png'),
-            radius: 72,
+            radius: 82,
           ),
         ),
         const SizedBox(height: 4),
         const Text(
           'Daffa Ilhami',
-          style: TextStyle(fontSize: 32),
+          style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
         ),
-        const SizedBox(height: 2),
         RichText(
+          textAlign: TextAlign.center,
           text: TextSpan(
             style:
                 Theme.of(context).textTheme.bodySmall?.copyWith(fontSize: 14),
             children: [
-              const TextSpan(text: '<'),
-              TextSpan(
-                  text: 'code',
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.secondaryContainer)),
-              const TextSpan(text: '> Programmer'),
-              TextSpan(
-                  text: ' | ',
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.secondaryContainer)),
-              const TextSpan(text: 'Speedcuber </'),
-              TextSpan(
-                  text: 'code',
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.secondaryContainer)),
-              const TextSpan(text: '>'),
+              const TextSpan(text: 'Programmer'),
+              getDivider(context),
+              const TextSpan(text: 'Speedcuber'),
             ],
           ),
         ),
