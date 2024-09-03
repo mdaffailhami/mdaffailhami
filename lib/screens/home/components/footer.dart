31676c3ade1ada92f2c49ec7b6a3ef50243854b7
mdaffailhami <mdaffailhami@gmail.com>
2024-09-03 08:43:26 +0800

    Update 2.2.1+10

--- lib/screens/home/components/footer.dart
+++ lib/screens/home/components/footer.dart
@@ -181,7 +181,7 @@ class MyFooter extends StatelessWidget {
                 child: const Divider(),
               ),
               SelectableText(
-                '| Last updated on August 8, 2024 |',
+                '| Last updated on September 3, 2024 |',
                 textAlign: TextAlign.center,
                 style: Theme.of(context)
                     .textTheme
