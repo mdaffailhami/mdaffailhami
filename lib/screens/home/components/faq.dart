16a696cf27aecad7a5f5a27e006a427687437577
Daffa Ilhami <mdaffailhami@gmail.com>
2024-10-17 10:57:17 +0700

    Fix maxwidth of MyProjects and FAQ

--- lib/screens/home/components/faq.dart
+++ lib/screens/home/components/faq.dart
@@ -15,43 +15,46 @@ class MyFAQ extends StatelessWidget {
       children: [
         Text('FAQ', style: Theme.of(context).textTheme.headlineLarge),
         const Divider(),
-        const Wrap(
-          alignment: WrapAlignment.center,
-          spacing: 20,
-          runSpacing: 12,
-          children: [
-            MyQnACard(
-              question: 'How do you learn programming?',
-              answer:
-                  "I learn it by myself, since I'm a self-learning master :)",
-            ),
-            MyQnACard(
-              question: 'When did you start learning programming?',
-              answer: 'I started learning programming on September 12, 2020.',
-            ),
-            MyQnACard(
-              question:
-                  "Is it true that you're currently speedrunning data science?",
-              answer:
-                  "Yes, it is. I just started the journey on July 9, 2024, and the reason is simply because right now I'm interested in it.",
-            ),
-            MyQnACard(
-              question:
-                  "How's the progress? Your data science speedrun journey.",
-              answer:
-                  "Well, it's pretty good. I've learned some math, statistics, NumPy, Pandas, Plotly, data cleaning, PostgreSQL, etc.",
-            ),
-            MyQnACard(
-              question: 'What are your hobbies?',
-              answer:
-                  "Thinking, pondering, self-learning, programming, cubing, and listening to Qur'an recitations.",
-            ),
-            MyQnACard(
-              question: "How fast can you solve a Rubik's Cube?",
-              answer:
-                  "My main cube is the X-Man Tornado V3 Pioneer UV, and currently I'm averaging below 15 seconds.",
-            ),
-          ],
+        Container(
+          constraints: const BoxConstraints(maxWidth: 1000),
+          child: const Wrap(
+            alignment: WrapAlignment.center,
+            spacing: 20,
+            runSpacing: 12,
+            children: [
+              MyQnACard(
+                question: 'How do you learn programming?',
+                answer:
+                    "I learn it by myself, since I'm a self-learning master :)",
+              ),
+              MyQnACard(
+                question: 'When did you start learning programming?',
+                answer: 'I started learning programming on September 12, 2020.',
+              ),
+              MyQnACard(
+                question:
+                    "Is it true that you're currently speedrunning data science?",
+                answer:
+                    "Yes, it is. I just started the journey on July 9, 2024, and the reason is simply because right now I'm interested in it.",
+              ),
+              MyQnACard(
+                question:
+                    "How's the progress? Your data science speedrun journey.",
+                answer:
+                    "Well, it's pretty good. I've learned some math, statistics, NumPy, Pandas, Plotly, data cleaning, PostgreSQL, etc.",
+              ),
+              MyQnACard(
+                question: 'What are your hobbies?',
+                answer:
+                    "Thinking, pondering, self-learning, programming, cubing, and listening to Qur'an recitations.",
+              ),
+              MyQnACard(
+                question: "How fast can you solve a Rubik's Cube?",
+                answer:
+                    "My main cube is the X-Man Tornado V3 Pioneer UV, and currently I'm averaging below 15 seconds.",
+              ),
+            ],
+          ),
         ),
       ],
     );
--- lib/screens/home/components/projects.dart
+++ lib/screens/home/components/projects.dart
@@ -15,104 +15,107 @@ class MyProjects extends StatelessWidget {
       children: [
         Text('My Projects', style: Theme.of(context).textTheme.headlineLarge),
         const Divider(),
-        const Wrap(
-          alignment: WrapAlignment.center,
-          spacing: 14,
-          runSpacing: 14,
-          children: [
-            MyProjectCard(
-              image: AssetImage(
-                'assets/project_logos/data-science-speedrun-journey-logo.png',
+        Container(
+          constraints: const BoxConstraints(maxWidth: 1000),
+          child: const Wrap(
+            alignment: WrapAlignment.center,
+            spacing: 14,
+            runSpacing: 14,
+            children: [
+              MyProjectCard(
+                image: AssetImage(
+                  'assets/project_logos/data-science-speedrun-journey-logo.png',
+                ),
+                title: 'Data Science Speedrun',
+                caption:
+                    "A repository contains notebooks and projects related to my data science speedrun journey.",
+                url:
+                    'https://github.com/mdaffailhami/data_science_speedrun_journey',
               ),
-              title: 'Data Science Speedrun',
-              caption:
-                  "A repository contains notebooks and projects related to my data science speedrun journey.",
-              url:
-                  'https://github.com/mdaffailhami/data_science_speedrun_journey',
-            ),
-            MyProjectCard(
-              image: AssetImage(
-                'assets/project_logos/gasku-logo.png',
+              MyProjectCard(
+                image: AssetImage(
+                  'assets/project_logos/gasku-logo.png',
+                ),
+                title: 'GasKu',
+                caption:
+                    'GasKu is an innovative application designed to streamline and secure the distribution of 3 kg subsidized LPG gas in Indonesia.',
+                url: 'https://github.com/mdaffailhami/gasku',
               ),
-              title: 'GasKu',
-              caption:
-                  'GasKu is an innovative application designed to streamline and secure the distribution of 3 kg subsidized LPG gas in Indonesia.',
-              url: 'https://github.com/mdaffailhami/gasku',
-            ),
-            MyProjectCard(
-              image: AssetImage(
-                'assets/project_logos/marketku-logo.png',
+              MyProjectCard(
+                image: AssetImage(
+                  'assets/project_logos/marketku-logo.png',
+                ),
+                title: 'MarketKu',
+                caption:
+                    'MarketKu is a products and services e-commerce application designed to provide convenient shopping experience.',
+                url: 'https://github.com/mdaffailhami/marketku',
               ),
-              title: 'MarketKu',
-              caption:
-                  'MarketKu is a products and services e-commerce application designed to provide convenient shopping experience.',
-              url: 'https://github.com/mdaffailhami/marketku',
-            ),
-            MyProjectCard(
-              image: AssetImage(
-                'assets/project_logos/mdi-todo-logo.png',
+              MyProjectCard(
+                image: AssetImage(
+                  'assets/project_logos/mdi-todo-logo.png',
+                ),
+                title: 'MDI Todo',
+                caption:
+                    "MDI Todo is a to-do list application designed to help you stay organized and on top of your tasks.",
+                url: 'https://github.com/mdaffailhami/mdi-todo',
               ),
-              title: 'MDI Todo',
-              caption:
-                  "MDI Todo is a to-do list application designed to help you stay organized and on top of your tasks.",
-              url: 'https://github.com/mdaffailhami/mdi-todo',
-            ),
-            MyProjectCard(
-              image: AssetImage(
-                'assets/project_logos/nasi-igut-han-logo.jpg',
+              MyProjectCard(
+                image: AssetImage(
+                  'assets/project_logos/nasi-igut-han-logo.jpg',
+                ),
+                title: 'Nasi Igut Han',
+                caption:
+                    'Nasi Igut Han is web application designed as an information system for the Nasi Igut Han rice ball business.',
+                url: 'https://github.com/mdaffailhami/nasi_igut_han',
               ),
-              title: 'Nasi Igut Han',
-              caption:
-                  'Nasi Igut Han is web application designed as an information system for the Nasi Igut Han rice ball business.',
-              url: 'https://github.com/mdaffailhami/nasi_igut_han',
-            ),
-            MyProjectCard(
-              image: AssetImage(
-                'assets/project_logos/glovikulator-logo.jpg',
+              MyProjectCard(
+                image: AssetImage(
+                  'assets/project_logos/glovikulator-logo.jpg',
+                ),
+                title: 'Glovikulator',
+                caption:
+                    'Glovikulator is a user-friendly calculator app. This is also my first app that I built and released using Flutter.',
+                url: 'https://github.com/mdaffailhami/glovikulator',
               ),
-              title: 'Glovikulator',
-              caption:
-                  'Glovikulator is a user-friendly calculator app. This is also my first app that I built and released using Flutter.',
-              url: 'https://github.com/mdaffailhami/glovikulator',
-            ),
-            MyProjectCard(
-              image: AssetImage(
-                'assets/project_logos/typing-practice-logo.png',
+              MyProjectCard(
+                image: AssetImage(
+                  'assets/project_logos/typing-practice-logo.png',
+                ),
+                title: 'Typing Practice',
+                caption:
+                    'Typing Practice is a web application designed to help users improve their typing skills through interactive exercises.',
+                url: 'https://github.com/mdaffailhami/typing-practice',
               ),
-              title: 'Typing Practice',
-              caption:
-                  'Typing Practice is a web application designed to help users improve their typing skills through interactive exercises.',
-              url: 'https://github.com/mdaffailhami/typing-practice',
-            ),
-            MyProjectCard(
-              image: AssetImage(
-                'assets/project_logos/customer-data-analysis-logo.png',
+              MyProjectCard(
+                image: AssetImage(
+                  'assets/project_logos/customer-data-analysis-logo.png',
+                ),
+                title: 'Customer Analysis',
+                caption:
+                    "Data cleaning and data analysis on customer data to gain insights into customer profiles and contact preferences.",
+                url: 'https://github.com/mdaffailhami/customer-data-analysis/',
               ),
-              title: 'Customer Analysis',
-              caption:
-                  "Data cleaning and data analysis on customer data to gain insights into customer profiles and contact preferences.",
-              url: 'https://github.com/mdaffailhami/customer-data-analysis/',
-            ),
-            MyProjectCard(
-              image: AssetImage(
-                'assets/project_logos/king-county-homesales-analysis-logo.png',
+              MyProjectCard(
+                image: AssetImage(
+                  'assets/project_logos/king-county-homesales-analysis-logo.png',
+                ),
+                title: 'Homesales Analysis',
+                caption:
+                    "Data analysis on home sales data in King County, United States, featuring geospatial mapping to visualize trends and factors influencing housing prices.",
+                url:
+                    'https://github.com/mdaffailhami/king_county_home_sales_analysis',
               ),
-              title: 'Homesales Analysis',
-              caption:
-                  "Data analysis on home sales data in King County, United States, featuring geospatial mapping to visualize trends and factors influencing housing prices.",
-              url:
-                  'https://github.com/mdaffailhami/king_county_home_sales_analysis',
-            ),
-            // MyProjectCard(
-            //   image: AssetImage(
-            //     'assets/project_logos/mdi-blog-logo.png',
-            //   ),
-            //   title: 'MDI Blog',
-            //   caption:
-            //       'MDI Blog is a web application that I created when I was studying CRUD. It does work, although the contents are so random.',
-            //   url: 'https://blog-mdaffailhami.web.app',
-            // ),
-          ],
+              // MyProjectCard(
+              //   image: AssetImage(
+              //     'assets/project_logos/mdi-blog-logo.png',
+              //   ),
+              //   title: 'MDI Blog',
+              //   caption:
+              //       'MDI Blog is a web application that I created when I was studying CRUD. It does work, although the contents are so random.',
+              //   url: 'https://blog-mdaffailhami.web.app',
+              // ),
+            ],
+          ),
         ),
       ],
     );
