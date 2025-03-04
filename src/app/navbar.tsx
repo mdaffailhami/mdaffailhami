629f330e27f97500f5dedaf1c98f54262846e7d5
Daffa Ilhami <mdaffailhami@gmail.com>
2025-03-04 08:07:58 +0800

    Add navbar transition

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -5,14 +5,12 @@ import React from 'react';
 
 type Link = {
   name: string;
-  icon?: React.ReactNode;
   path: string;
 };
 
 const links: Link[] = [
   {
     name: 'Home',
-    // icon: <IoHomeOutline className='inline' />,
     path: '/',
   },
   {
@@ -40,25 +38,78 @@ const links: Link[] = [
 
 export default function Navbar() {
   const pathname = usePathname();
+  const [update, forceUpdate] = React.useState(0); // Dummy state to trigger re-renders
 
-  console.log(pathname);
+  const navbarRef = React.useRef<HTMLElement | null>(null);
+  const activeNavLinkRef = React.useRef<HTMLAnchorElement | null>(null);
+  const activeIndicatorNavLinkRef = React.useRef<HTMLDivElement | null>(null);
+
+  React.useLayoutEffect(() => {
+    console.log('MASUKK');
+    activeIndicatorNavLinkRef.current!.style.width =
+      activeNavLinkRef.current!.offsetWidth + 'px';
+    activeIndicatorNavLinkRef.current!.style.height =
+      activeNavLinkRef.current!.offsetHeight + 'px';
+
+    const navbarPaddingLeft = parseFloat(
+      window.getComputedStyle(navbarRef.current!).paddingLeft,
+    );
+
+    activeIndicatorNavLinkRef.current!.style.transform = `translateX(${activeNavLinkRef.current!.offsetLeft - navbarPaddingLeft}px)`;
+  }, [update, pathname]);
+
+  React.useEffect(() => {
+    const observer = new ResizeObserver(() => forceUpdate((n) => n + 1));
+    observer.observe(navbarRef.current!);
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
-    <nav className='bg-surface border-on-background fixed top-2.5 left-1/2 -translate-x-1/2 space-x-2 rounded-full border px-2.5 py-3.5'>
-      {links.map((link, i) => (
-        <NavLink key={i + 1} isActive={link.path === pathname} link={link} />
-      ))}
+    <nav
+      ref={navbarRef}
+      className='bg-surface border-on-background fixed top-2.5 left-1/2 -translate-x-1/2 rounded-full border px-2.5 py-3.5'
+    >
+      <ul className='space-x-2'>
+        {links.map((link, i) => (
+          <li key={i + 1} className='inline'>
+            <NavLink
+              ref={link.path == pathname ? activeNavLinkRef : null}
+              isActive={link.path === pathname}
+              link={link}
+            />
+          </li>
+        ))}
+      </ul>
+      <div
+        ref={activeIndicatorNavLinkRef}
+        className={`outline-primary absolute top-2.5 rounded-full outline-2 transition duration-200 ease-linear`}
+      />
     </nav>
   );
 }
 
-function NavLink(props: { isActive: boolean; link: Link }) {
+function NavLink(
+  props: { isActive: boolean; link: Link } & React.ComponentPropsWithRef<'a'>,
+) {
   return (
     <Link
+      ref={props.ref}
       href={props.link.path}
-      className={`hover:bg-primary/15 rounded-full px-3 py-1.5 font-medium ${props.isActive ? 'outline-primary text-on-surface outline-2' : 'text-on-surface/85'}`}
+      onClick={props.onClick}
+      className={`hover:bg-primary/15 rounded-full px-3 py-1.5 font-medium ${props.isActive ? 'text-on-surface' : 'text-on-surface/85'}`}
     >
-      {props.link.icon}
       {props.link.name}
     </Link>
   );
 }
+// function NavLink(props: { isActive: boolean; link: Link }) {
+//   return (
+//     <Link
+//       href={props.link.path}
+//       className={`hover:bg-primary/15 inline-block rounded-full px-3 py-1.5 font-medium transition ${props.isActive ? 'outline-primary text-on-surface outline-2' : 'text-on-surface/85'}`}
+//     >
+//       {props.link.name}
+//     </Link>
+//   );
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
+import React from 'react';
 export default function HomePage() {
   return (
-    <>
-      <main className='flex h-[100vh] items-center justify-center bg-black'>
-        <h1 className='text-center text-4xl font-medium text-white'>
-          Hello Deno + Next.js + Tailwind CSS
-        </h1>
-      </main>
-    </>
+    <main className='flex min-h-screen items-center justify-center bg-black'>
+      <h1 className='text-center text-4xl font-medium text-white'>
+        Hello Deno + Next.js + Tailwind CSS
+      </h1>
+    </main>
   );
 }
