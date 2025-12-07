b7168fda5e265dca40b82403e1e258697652ff89
Daffa Ilhami <mdaffailhami@gmail.com>
2025-12-07 22:38:55 +0800

    Update home slide

--- src/app/(main)/home/slide.tsx
+++ src/app/(main)/home/slide.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import { Slide } from "@/components/slide";
+import { config } from "@/database/data/config";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { UserCircle2Icon } from "lucide-react";
+import { GetToKnowButton } from "./get-to-know-button";
 
 export default function HomeSlide() {
   return (
     <Slide id="home">
-      <div className="w-[92%] mx-auto flex flex-col md:flex-row-reverse items-center min-h-full gap-8">
-        <section className="w-full md:flex-2 flex justify-center">
+      <div className="w-full max-w-[92%] md:max-w-[80%] mx-auto flex flex-col md:flex-row-reverse items-center min-h-full">
+        <section className="w-full md:flex-2 flex justify-center md:justify-end">
           <Image
             src="/mdaffailhami-1.png"
             alt="Daffa Ilhami (1)"
@@ -21,16 +26,14 @@ export default function HomeSlide() {
             className="rounded-b-full w-[75%] md:w-md md:hidden"
           />
         </section>
-        <section className="flex flex-col w-full md:flex-3">
-          <h1 className="font-serif text-[4rem] font-medium leading-18">
+        <section className="flex flex-col w-full md:flex-3 items-start">
+          <h1 className="font-serif text-5xl md:text-[4rem] font-medium leading-18">
             Daffa Ilhami
           </h1>
-          <p className="text-justify text-lg text-muted-foreground">
-            UWU Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-            minus corporis eos fuga? Rem error reiciendis, eveniet temporibus
-            vel possimus dolores iusto, unde deleniti corporis qui ducimus
-            dolorem dignissimos numquam!
+          <p className="text-left text-base md:text-lg text-muted-foreground">
+            {config.bio}
           </p>
+          <GetToKnowButton />
         </section>
       </div>
     </Slide>
---
+++ src/database/data/config.ts
@@ -0,0 +1,25 @@
+const about = `\
+## Hi, I’m **Daffa Ilhami** 👋
+
+I’m a **self-taught programmer** from **Indonesia** 🇮🇩 with over **5 years** of experience in the field.
+
+I started **programming** while I was still in **madrasah aliyah** 🏫 — so the trigger was **my own interest**, not college.
+
+I’ve experimented with a **wide range of technologies** ⚙️ — including *React*, *Flutter*, *React Native*, *Svelte*, *Express*, *FastAPI*, *Firebase*, *MongoDB*, *PostgreSQL*, **and more**.
+
+I’ve taught myself **various skills** 📚 over the years, and every day brings a new opportunity to *explore*, *create*, and *push the limits of what I know*.
+
+---
+
+My journey is driven by **self-learning**, **curiosity**, and a **commitment to continuous growth** 🚀
+
+---
+
+Here are some of the **technologies** that I use:
+
+📝 **Note:** *These are the technologies **I'm currently actively using**, not a full representation of my skills.*`;
+
+export const config = {
+  bio: "Tech Enthusiast | Self-Taught Programmer | Lifelong Learner",
+  about: about,
+};
