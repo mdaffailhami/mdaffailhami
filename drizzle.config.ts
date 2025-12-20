1821246f926ca112e2d150e63ab00ce3a399afa6
Daffa Ilhami <mdaffailhami@gmail.com>
2025-12-20 22:38:28 +0800

    Initialize drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./src/lib/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+});
