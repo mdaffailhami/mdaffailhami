7b13c5b2343b44c83f55b3d467f22b2a0041852e
Daffa Ilhami <mdaffailhami@gmail.com>
2025-12-30 15:06:07 +0800

    Automatically open video overlay after 8 seconds of the page load

--- src/app/(main)/(home)/call-to-action-button.tsx
+++ src/app/(main)/(home)/call-to-action-button.tsx
@@ -3,14 +3,31 @@
 import { Button } from "@/components/ui/button";
 import { CodeIcon } from "lucide-react";
 import { OverlayView } from "@/components/common/overlay-view";
+import { useEffect, useRef } from "react";
 
 export function CallToActionButton() {
+  const everOpened = useRef(false);
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      // As long as the user has never clicked the button yet,
+      // open the overlay automatically after 8 seconds
+      if (!everOpened.current) {
+        document.getElementById("cta")?.click();
+      }
+    }, 8 * 1000);
+
+    // Clear the timeout when the component unmounts
+    return () => clearTimeout(timeOut);
+  }, []);
+
   return (
     <OverlayView
       className="w-full"
       trigger={
         <Button
-          onClick={() => {}}
+          id="cta"
+          onClick={() => (everOpened.current = true)}
           className="animate-bounce fl-p-3/5! bg-primary group text-primary-foreground hover:bg-primary/5! hover:border-primary hover:text-primary transition-colors border-border border-2"
         >
           {/* <UserCircle2Icon className="size-5 md:size-6 group-hover:scale-105 transition-transform group-hover:-translate-x-1" /> */}
